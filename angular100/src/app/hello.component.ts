d47d8be033aaca3dabef97376e74a8e6fcdb7bdc
QuangNX <adquang199x@gmail.com>
2020-07-15 14:13:30 +0700

    day 4 ng-if in angular

--- angular100/src/app/hello.component.ts
+++ angular100/src/app/hello.component.ts
@@ -5,12 +5,18 @@ import { Component } from '@angular/core';
             <p>Your age : {{user.age}}</p>
             <button (click)="showInfo()">Click me</button>
             <input type="text" [(ngModel)]="user.name"/>
+            <p *ngIf="user.age>13;else noSee">
+            You can see this movie</p>
+            <ng-template #noSee>
+            <p>
+            You can not see this movie</p>
+            </ng-template>
             `,
 })
 export class HelloComponent {
   user = {
     name: 'quang',
-    age: 21
+    age: 10
   };
   showInfo(): void {
     alert('hello');
