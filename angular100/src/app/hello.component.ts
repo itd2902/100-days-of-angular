6402eec4e7a76331cf87c2cf60feab8879b58239
QuangNX <adquang199x@gmail.com>
2020-07-15 15:11:23 +0700

    day 5

--- angular100/src/app/hello.component.ts
+++ angular100/src/app/hello.component.ts
@@ -11,6 +11,27 @@ import { Component } from '@angular/core';
             <p>
             You can not see this movie</p>
             </ng-template>
+            <div *ngFor="let author of authors;index as idx; count as total, first as first, last as last, even as even; odd as odd">
+  STT : {{idx}}-Total : {{total}}-First : {{first}}-Last : {{last}}-Even : {{even}}-Odd:{{odd}}-{{author.id}} - {{author.firstName}} {{author.lastName}}
+  <ng-template *ngIf="user.age > 10 then goHome">
+    <div>Hello</div>
+  </ng-template>
+  <ng-template #goHome>
+    <div>Hello 2</div>
+  </ng-template>
+</div>
+<ng-template
+  ngFor
+  [ngForOf]="authors"
+  let-author
+  let-idx="index"
+  let-total="count"
+>
+  <div>
+    ({{idx}})/({{total}}): {{author.id}} - {{author.firstName}}
+    {{author.lastName}}
+  </div>
+</ng-template>
             `,
 })
 export class HelloComponent {
@@ -21,4 +42,23 @@ export class HelloComponent {
   showInfo(): void {
     alert('hello');
   }
+  authors = [
+    {
+      id: 1,
+      firstName: "Flora",
+      lastName: "Twell",
+      email: "ftwell0@phoca.cz",
+      gender: "Female",
+      ipAddress: "99.180.237.33",
+    },
+    {
+      id: 2,
+      firstName: "Priscella",
+      lastName: "Signe",
+      email: "psigne1@berkeley.edu",
+      gender: "Female",
+      ipAddress: "183.243.228.65",
+    },
+    // more data
+  ];
 }
